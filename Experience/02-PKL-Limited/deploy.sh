a4d331cda0481de5cc2e68b04e447cdd6fcf63de
sandya kandregula <52678059+sandy2341@users.noreply.github.com>
2026-04-02 13:06:47 +0530

    Create deploy.sh
    Signed-off-by: sandya kandregula <52678059+sandy2341@users.noreply.github.com>

---
+++ Experience/02-PKL-Limited/deploy.sh
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PKL Website</title>
+    <style>
+        body {
+            font-family: Arial;
+            text-align: center;
+            margin-top: 50px;
+        }
+        h1 {
+            color: #0073e6;
+        }
+    </style>
+</head>
+<body>
+
+<h1>Welcome to PKL Website</h1>
+<p>Hosted on AWS</p>
+<p>Deployed using cloud infrastructure</p>
+
+</body>
+</html>
